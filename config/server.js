0ae3b88f09afec88f57a235293cff8746a3a786e
Marcela Conejo <mconejo@commonbond.co>
2017-12-29 10:04:53 -0600

    Add CORS

--- config/server.js
+++ config/server.js
@@ -1,12 +1,15 @@
 'use strict';
 
 import Hapi from 'hapi';
+import hapi_cors_headers from "hapi-cors-headers";
 
 const server = new Hapi.Server();
 
 server.connection({port: process.env.PORT || 3000 });
 
 require('../app/routes/index.js')(server);
+
+server.ext('onPreResponse', hapi_cors_headers)
 
 server.start((err) => {
   if(err){
